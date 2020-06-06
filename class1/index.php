e380384bafe9bfd7279a06c609c750d8fefce9c7
Ariful <arif98741@gmail.com>
2020-06-06 21:09:28 +0600

    switch case don

---
+++ class1/index.php
@@ -0,0 +1,19 @@
+<?php 
+//1.pre-defined function
+//2. user defined function
+// if else
+$x = 2;
+switch($x)
+{
+    case 1:
+        echo 'hello';
+        break;
+    case 2:
+        echo 'welcome';   
+        break;
+    case 3:
+            echo 'intro'; 
+            break;
+    default:
+         echo 'nothing';     
+}
